a9092bdf12fccc680a52fedf7b522ddb58c4e8f5
João Goulart <jv.goulart.almeida@hotmail.com>
2019-12-18 09:18:52 -0300

    files renamed

--- functions/controller/field.controller.js
+++ functions/controller/field.controller.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const fieldService = require('../service/fieldService')
+const fieldService = require('../service/field.service')
 
 
 
--- functions/functions/field.js
+++ functions/functions/field.js
@@ -4,6 +4,6 @@ const serverless = require('serverless-http')
 const app = require('../app')
 
 
-require('../controller/fieldController')(app)
+require('../controller/field.controller')(app)
 
 module.exports.handler = serverless(app)
